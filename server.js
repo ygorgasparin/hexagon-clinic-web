44880a9f8f426b222542f8620597d53b2f2bbe68
ygor.gasparin <ygor.gasparin@gmail.com>
2017-07-12 09:21:33 -0300

    Fixed url server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
