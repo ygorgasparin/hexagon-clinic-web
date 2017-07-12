2d1a9dba661d261c47286205b78bf5f9b8e03176
ygor.gasparin <ygor.gasparin@gmail.com>
2017-07-11 23:40:26 -0300

    Cors

--- server.js
+++ server.js
@@ -2,13 +2,6 @@
 const express = require('express');
 const app = express();
 
-app.all('*', function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  }
-);
-
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
